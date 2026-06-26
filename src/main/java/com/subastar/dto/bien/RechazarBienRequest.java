7659beb63d7c7cc211f37ed321d8d1933da566b5
Magali <maguialfie@gmail.com>
2026-06-25 21:57:12 -0300

    fix: costo_devolucion opcional al rechazar bien; flyway enabled por defecto

--- src/main/java/com/subastar/dto/bien/RechazarBienRequest.java
+++ src/main/java/com/subastar/dto/bien/RechazarBienRequest.java
@@ -2,7 +2,6 @@ package com.subastar.dto.bien;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import java.math.BigDecimal;
 
@@ -12,7 +11,6 @@ public class RechazarBienRequest {
     @NotBlank(message = "El motivo de rechazo es obligatorio")
     private String motivo;
 
-    @NotNull(message = "El costo de devolución es obligatorio")
     @JsonProperty("costo_devolucion")
     private BigDecimal costoDevolucion;
 }
